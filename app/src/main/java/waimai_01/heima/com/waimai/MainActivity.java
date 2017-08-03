538e4e2c72b49daf84180fcc681526f01f7eed71
laidou <564379170@qq.com>
2017-08-03 09:38:42 +0800

    Initial commit

---
+++ app/src/main/java/waimai_01/heima/com/waimai/MainActivity.java
@@ -0,0 +1,13 @@
+package waimai_01.heima.com.waimai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
